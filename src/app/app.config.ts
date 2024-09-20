67a50549a821b81e51964352f2ad941c70b283c8
Felipe Lima <pegasusyt1@gmail.com>
2024-09-20 16:44:50 -0300

    adiciona as stars de 0 a 5 para notas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';  // Importa withFetch
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
+import { RatingModule } from 'primeng/rating';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch()),  // Adiciona withFetch ao HttpClient
+    provideHttpClient(withFetch()),
+    importProvidersFrom(RatingModule), 
   ],
 };
--- src/app/components/book-card/book-card.component.ts
+++ src/app/components/book-card/book-card.component.ts
@@ -1,14 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Book } from '../../models/book.model';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-card',
   standalone: true,
   templateUrl: './book-card.component.html',
   styleUrls: ['./book-card.component.scss'],
+  imports: [RatingModule, FormsModule],
 })
 export class BookCardComponent {
   @Input() book!: Book;
+  value: number = 0; 
 
   addToFavorites() {
     console.log(`Adicionado aos favoritos: ${this.book.volumeInfo.title}`);
