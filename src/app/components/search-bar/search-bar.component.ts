7db865f4898184f1f5fe49cf89a5875cd6fd6075
Felipe Lima <pegasusyt1@gmail.com>
2024-09-20 13:23:57 -0300

    implementacao de rxjs para pesquisar automaticamente apos a pessoa parar de digitar em 1500ms

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,17 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {
+  Subject,
+  Subscription,
+  debounceTime,
+  distinctUntilChanged,
+} from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,11 +20,29 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./search-bar.component.scss'],
   imports: [FormsModule],
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit, OnDestroy {
   query: string = '';
   @Output() search = new EventEmitter<string>(); // O tipo é string
 
   onSearch() {
     this.search.emit(this.query); // Emitindo a string digitada
+  }
+  private searchSubject = new Subject<string>();
+  private subscription!: Subscription;
+
+  ngOnInit() {
+    this.subscription = this.searchSubject
+      .pipe(debounceTime(1500), distinctUntilChanged())
+      .subscribe((query) => {
+        this.search.emit(query);
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  onInputChange() {
+    this.searchSubject.next(this.query);
   }
 }
